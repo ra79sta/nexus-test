1f10092605786bb296355719963acdc3b3d57ccf
Stanoje Radivojevic <staca_r@hotmail.com>
2024-11-16 02:14:06 +0100

    remove

--- src/plugins/apiWrapper.ts
+++ src/plugins/apiWrapper.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const isDevelopment = import.meta.env.MODE === "development"
 
-const baseURL = isDevelopment ? "/api" : "/api/proxy"
+const baseURL = isDevelopment ? "/api" : import.meta.env.VITE_API_URL
 
 const axiosInstance = axios.create({
   baseURL,
@@ -12,14 +12,14 @@ const axiosInstance = axios.create({
   },
 })
 
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 404) {
-//       window.location.href = "/404"
-//     }
-//     return Promise.reject(error)
-//   },
-// )
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 404) {
+      window.location.href = "/404"
+    }
+    return Promise.reject(error)
+  },
+)
 
 export default axiosInstance
