1c4aa8bfaf5dccfbbbc01a7a6693527e720ec59b
Stanoje Radivojevic <staca_r@hotmail.com>
2024-11-18 11:59:26 +0100

    add types in vehicle services

--- src/services/vehicleService.ts
+++ src/services/vehicleService.ts
@@ -1,22 +1,40 @@
 import apiInstance from "@/plugins/apiWrapper"
 
+export interface Year {
+  year: string | number
+}
+
+export interface Make {
+  make: string
+}
+
+export interface VehicleModel {
+  vehicle_id: string
+  type: string
+  vehicle_type: string
+  body_type: string
+  year: number
+  make: string
+  model: string
+  original_model: string
+}
+
+const API_TOKEN = import.meta.env.VITE_API_URL_TOKEN as string
+
 export default {
-  async getYears() {
-    const response = await apiInstance.get(`v2/vehicles/years/?token=${import.meta.env.VITE_API_URL_TOKEN as string}`)
+  async getYears(): Promise<Year[]> {
+    const response = await apiInstance.get(`v2/vehicles/years/?token=${API_TOKEN}`)
+    console.log("response", response)
     return response.data
   },
 
-  async getMakes(year: string) {
-    const response = await apiInstance.get(
-      `v2/vehicles/makes/?year=${year}&token=${import.meta.env.VITE_API_URL_TOKEN as string}`,
-    )
+  async getMakes(year: string): Promise<Make[]> {
+    const response = await apiInstance.get(`v2/vehicles/makes/?year=${year}&token=${API_TOKEN}`)
     return response.data
   },
 
-  async getModels(year: string, make: string) {
-    const response = await apiInstance.get(
-      `v2/vehicles/models/?year=${year}&make=${make}&token=${import.meta.env.VITE_API_URL_TOKEN as string}`,
-    )
+  async getModels(year: string, make: string): Promise<VehicleModel[]> {
+    const response = await apiInstance.get(`v2/vehicles/models/?year=${year}&make=${make}&token=${API_TOKEN}`)
     return response.data
   },
 }
